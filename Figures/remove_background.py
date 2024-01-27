abb895707f037aaa043e82b6303ae4f199e98aa5
Astro-Coco <colinc131@gmail.com>
2024-01-27 11:47:17 -0500

    removed_background

---
+++ Figures/remove_background.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import numpy as np
+
+def remove_background_with_text_preservation(image):
+    # Convert the image to RGBA mode if not already
+    if image.mode != 'RGBA':
+        image = image.convert('RGBA')
+
+    # Get the image data as a numpy array
+    img_data = np.array(image)
+
+    # Define the RGB values for the white background
+    white_color = [255, 255, 255]
+
+    # Create a mask to identify white pixels
+    mask = np.all(img_data[:, :, :3] == white_color, axis=-1)
+
+
+    # Apply the mask to the image data
+    result_data = img_data.copy()
+    result_data[mask] = [0, 0, 0, 0]  # Set RGBA values to transparent for masked pixels
+
+    # Create a new PIL Image from the result data
+    result_image = Image.fromarray(result_data)
+
+    return result_image
+
+
+if __name__ == 'main':
+
+    images_to_clear = ['besoins.png', 'caractéristiques.png', 'Fonctions.png']
+
+    for image_path in images_to_clear:
+        with Image.open(image_path) as img:
+            result = remove_background_with_text_preservation(img)
+            result.save(f"processed_{image_path}")
